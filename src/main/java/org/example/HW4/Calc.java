818f3c7f487129bbe2a73b2c6048cbb9983d30ff
LAPTOP-100802\yulia.volkova <yulia.volkova@hys-enterprise.com>
2023-04-09 14:33:44 +0200

    HW-3

---
+++ src/main/java/org/example/HW4/Calc.java
@@ -0,0 +1,24 @@
+package org.example.HW4;
+
+public class Calc {
+    public static double sum(int n, double m){
+        return n+m;
+
+    }
+
+    public static double min(int n, double m) {
+        return n-m;
+    }
+
+    public static double multip(int n,  double m) {
+        return n*m;
+    }
+
+    public static double div(int n, double m) {
+        return n/m;
+    }
+
+    public static double per(int n, double m) {
+        return n%m;
+    }
+}
---
+++ src/main/java/org/example/HW4/HW4.java
@@ -0,0 +1,37 @@
+package org.example.HW4;
+
+import java.util.Scanner;
+
+public class HW4 {public static void main(String[] args) {
+    Scanner scanner=new Scanner(System.in);
+    System.out.println("enter first number");
+    int number=scanner.nextInt();
+    double number2;
+    double result = 0;
+    char symbol;
+    System.out.println("enter second number");
+    number2=scanner.nextDouble();
+    System.out.println("enter symbol of operation");
+    symbol=scanner.next().charAt(0);
+    switch (symbol){
+        case '+': result=Calc.sum(number,number2);
+            break;
+        case '-': result=Calc.min(number,number2);
+            break;
+        case '*': result=Calc.multip(number,number2);
+            break;
+        case '/': result=Calc.div(number,number2);
+            break;
+        case '%': result=Calc.per(number,number2);
+            break;
+
+        default:
+            System.out.println("Error");
+            System.exit(0);
+            break;
+    }
+    System.out.println(result);
+}
+}
+
+
